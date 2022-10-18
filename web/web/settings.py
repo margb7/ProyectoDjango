ffcfe776677a8ac99ccc9314410a52030d147327
Mario González Besada <mariogonzalezgoian@gmail.com>
2022-10-18 14:15:28 +0200

    Django project

--- web/web/settings.py
+++ web/web/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'misitio.apps.MisitioConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
